df26a85db469b78a3d2e19172ab8d5f13810e233
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-28 20:07:04 +0530

    ✅ Maximum Element After Decreasing and Rearranging | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-06-28

--- leetcode/medium/1846-maximum-element-after-decreasing-and-rearranging/solution.java
+++ leetcode/medium/1846-maximum-element-after-decreasing-and-rearranging/solution.java
@@ -1,16 +1,40 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
 
-        arr[0]=1;
+
+
+
+        //Approach 1: Greedy approach O(N log N) and space complexity is O(1)
+        // Arrays.sort(arr);
+
+        // arr[0]=1;
+        // for(int i=1;i<arr.length;i++){
+        //     //arr[i]=Math.min(arr[i],arr[i-1]+1);
+        //     //or
+        //     if(arr[i]>arr[i-1]+1){
+        //     arr[i]=arr[i-1]+1;
+        //     }
+        // }
+
+        // return arr[arr.length-1];
+
+
+
+        //APPRAOCH 2 : T.C. O(n) but S.C. is O(N)
+
+        int count[]=new int[arr.length+1];
+        count[0]=1;
         for(int i=1;i<arr.length;i++){
-            //arr[i]=Math.min(arr[i],arr[i-1]+1);
-            //or
-            if(arr[i]>arr[i-1]+1){
-            arr[i]=arr[i-1]+1;
-            }
+            count[Math.min(arr[i],arr.length)]++;
         }
 
-        return arr[arr.length-1];
+        int ans = 0;
+
+        for (int i = 1; i <= arr.length; i++) {
+            ans = Math.min(ans + count[i], i);
+        }
+
+        return ans;
+
     }
 }
