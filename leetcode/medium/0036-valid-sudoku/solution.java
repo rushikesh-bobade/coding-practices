7abc236f62675301a642015cabed50bb00d22861
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-28 22:29:54 +0530

    ✅ Valid Sudoku | Medium | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-06-28

---
+++ leetcode/medium/0036-valid-sudoku/solution.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    public boolean isValidSudoku(char[][] board) {
+
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                char current = board[row][col];
+
+                if (current == '.')
+                    continue;
+
+                int box = (row / 3) * 3 + (col / 3);
+
+                if (rows[row].contains(current))
+                    return false;
+
+                if (cols[col].contains(current))
+                    return false;
+
+                if (boxes[box].contains(current))
+                    return false;
+
+                rows[row].add(current);
+                cols[col].add(current);
+                boxes[box].add(current);
+            }
+        }
+
+        return true;
+    }
+}
