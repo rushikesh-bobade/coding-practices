7c9614e4da43d28770d36fdf790ac7fceb51d644
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-28 19:44:35 +0530

    ✅ Maximum Element After Decreasing and Rearranging | Medium | Java | Runtime: 1 ms | Memory: 42.7 MB | 2026-06-28

--- leetcode/medium/1846-maximum-element-after-decreasing-and-rearranging/solution.java
+++ leetcode/medium/1846-maximum-element-after-decreasing-and-rearranging/solution.java
@@ -2,13 +2,11 @@ class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
         Arrays.sort(arr);
 
-        int min=1;
         arr[0]=1;
         for(int i=1;i<arr.length;i++){
-            min=Math.min(arr[i],arr[i-1]+1);
-            arr[i]=min;
+            arr[i]=Math.min(arr[i],arr[i-1]+1);
         }
 
-        return min;
+        return arr[arr.length-1];
     }
 }
