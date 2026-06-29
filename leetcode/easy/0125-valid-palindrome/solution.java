b7e85cf16aba2c76a5af788b0bb57ca114766924
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-29 23:31:12 +0530

    ✅ Valid Palindrome | Easy | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-06-29

---
+++ leetcode/easy/0125-valid-palindrome/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int l=0;
+        int r=s.length()-1;
+
+        while(l<r){
+
+            while(!Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+            }
+
+            while(!Character.isLetterOrDigit(s.charAt(r))){
+                r--;
+            }
+
+            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+
+
+        //Brute force approach using String Builder
+        // StringBuilder str=new StringBuilder();
+        // for(char c:s.toCharArray()){
+        //     if(Character.isLetterOrDigit(c)){
+        //         str.append(Character.toLowerCase(c));
+        //     }
+        // }
+
+        // String str1=str.toString();
+        // String str2=str.reverse().toString();
+
+        // return str1.equals(str2);
+    }
+}
