bb68eb37ad1b08e79478313c586bb14b033ea071
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-29 21:57:56 +0530

    ✅ Number of Strings That Appear as Substrings in Word | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-06-29

--- leetcode/easy/1967-number-of-strings-that-appear-as-substrings-in-word/solution.java
+++ leetcode/easy/1967-number-of-strings-that-appear-as-substrings-in-word/solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-        int count = 0;
+        int count=0;
 
-        for (String pattern : patterns) {
-            if (word.contains(pattern)) {
+        for(String pattern:patterns){
+            if(word.contains(pattern)){
                 count++;
             }
         }
-
         return count;
     }
 }
