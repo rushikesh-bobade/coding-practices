24088db87d247ddc72112ae46abbcf717e6643b6
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-07-03 17:13:45 +0530

    ✅ Maximum Subarray | Medium | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-07-03

--- leetcode/medium/0053-maximum-subarray/solution.java
+++ leetcode/medium/0053-maximum-subarray/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        int maxSum=Integer.MAX_VALUE;
+        int sum=nums[0];
 
-        int maxSum=Integer.MIN_VALUE;
-        int count=0;
-
-        for(int i =0;i<nums.length;i++){
-            count=count+nums[i];
-            maxSum=Math.max(maxSum,count);
-            if(count<0){
-                count=0;
+        for(int i=1;i<nums.length;i++){
+            sum=sum+nums[i];
+            maxSum=Math.max(sum,maxSum);
+            if(sum<0){
+                sum=0;
             }
         }
+        
     return maxSum;
     }
 }
