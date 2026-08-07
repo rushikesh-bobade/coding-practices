94cb1dddfd2036320d4b169f4d17a66710927d34
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-07 20:16:14 +0530

    ✅ Capitalize the Title | Easy | Java | Runtime: 3 ms (80.13%) | Memory: 43.9 MB | 2026-08-07

--- leetcode/easy/2129-capitalize-the-title/solution.java
+++ leetcode/easy/2129-capitalize-the-title/solution.java
@@ -1,30 +1,47 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        
-        String s[]=title.trim().split("\\s+");
-        StringBuilder sb=new StringBuilder();
 
-        for(int i=0;i<s.length;i++){
-            boolean j=true;
+        String[] words = title.toLowerCase().split(" ");
+        StringBuilder sb = new StringBuilder();
 
-            for(char c : s[i].toCharArray()){
-                if(s[i].length()==1){
-                    sb.append(Character.toLowerCase(c));
-                }else if(s[i].length()==2){
-                    sb.append(Character.toLowerCase(c));
-                }else{
-                    if(j){
-                        sb.append(Character.toUpperCase(c));
-                    }else{
-                        sb.append(Character.toLowerCase(c));
-                    }
-                    j=false;
-                }
-           }
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) sb.append(" ");
 
-           sb.append(" ");
+            if (words[i].length() <= 2) {
+                sb.append(words[i]);
+            } else {
+                sb.append(Character.toUpperCase(words[i].charAt(0)));
+                sb.append(words[i].substring(1));
+            }
         }
 
-        return sb.toString().trim();
+        return sb.toString();
+    
+
+        // String s[]=title.trim().split("\\s+");
+        // StringBuilder sb=new StringBuilder();
+
+        // for(int i=0;i<s.length;i++){
+        //     boolean j=true;
+
+        //     for(char c : s[i].toCharArray()){
+        //         if(s[i].length()==1){
+        //             sb.append(Character.toLowerCase(c));
+        //         }else if(s[i].length()==2){
+        //             sb.append(Character.toLowerCase(c));
+        //         }else{
+        //             if(j){
+        //                 sb.append(Character.toUpperCase(c));
+        //             }else{
+        //                 sb.append(Character.toLowerCase(c));
+        //             }
+        //             j=false;
+        //         }
+        //    }
+
+        //    sb.append(" ");
+        // }
+
+        // return sb.toString().trim();
     }
 }
