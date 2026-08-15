9ae5e210781949361cf9fa945fa7956f61c6cf31
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-15 19:44:00 +0530

    ✅ Frog Jump | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

--- gfg/medium/geek-jump/solution.java
+++ gfg/medium/geek-jump/solution.java
@@ -4,27 +4,49 @@ import java.io.*;
 class Solution {
   
     
-    int helper(int height[], int n, ArrayList<Integer>dp){
-        if(n==0) return 0;
-        if(dp.get(n)!=-1){
-            return dp.get(n);
-        }
-        int left=helper(height,n-1,dp)+Math.abs(height[n]-height[n-1]);
+    // int helper(int height[], int n, ArrayList<Integer>dp){
+    //     if(n==0) return 0;
+    //     if(dp.get(n)!=-1){
+    //         return dp.get(n);
+    //     }
+    //     int left=helper(height,n-1,dp)+Math.abs(height[n]-height[n-1]);
         
-        int right=Integer.MAX_VALUE;
-        if(n>1){
-        right=helper(height,n-2,dp)+Math.abs(height[n]-height[n-2]);
-        }
+    //     int right=Integer.MAX_VALUE;
+    //     if(n>1){
+    //     right=helper(height,n-2,dp)+Math.abs(height[n]-height[n-2]);
+    //     }
     
-        dp.set(n,Math.min(left,right));
-        return dp.get(n);
+    //     dp.set(n,Math.min(left,right));
+    //     return dp.get(n);
         
-    }
+    // }
+    
+    // int minCost(int[] height) {
+    // // code here
+    // int n=height.length;
+    // ArrayList<Integer>dp=new ArrayList<>(Collections.nCopies(n,-1));
+    // return helper(height,n-1,dp);
+    // }
     
     int minCost(int[] height) {
     // code here
     int n=height.length;
     ArrayList<Integer>dp=new ArrayList<>(Collections.nCopies(n,-1));
-    return helper(height,n-1,dp);
+    dp.set(0,0);
+    
+    for(int i=1;i<n;i++){
+        
+        
+    int left=dp.get(i-1) + Math.abs(height[i]-height[i-1]);
+    int right=Integer.MAX_VALUE;
+        
+        if(i>1){
+            right=dp.get(i-2) + Math.abs(height[i]-height[i-2]);
+        }
+        
+    dp.set(i,Math.min(left,right));
+    }
+    
+    return dp.get(n-1);
     }
 }
