ec9643c78bf2e005291738efff939bf7042065d0
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-16 21:17:54 +0530

    ✅ House Robber | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-16

--- leetcode/medium/0198-house-robber/solution.java
+++ leetcode/medium/0198-house-robber/solution.java
@@ -1,12 +1,34 @@
+//Maximum sum of non-adjacent elements
+
 class Solution {
     public int rob(int[] nums) {
-        int prev2 = 0;
-        int prev1 = 0;
-        for (int n : nums) {
-            int curr = Math.max(prev1, prev2 + n);
-            prev2 = prev1;
-            prev1 = curr;
+        // int prev2 = 0;
+        // int prev1 = 0;
+        // for (int n : nums) {
+        //     int curr = Math.max(prev1, prev2 + n);
+        //     prev2 = prev1;
+        //     prev1 = curr;
+        // }
+        // return prev1;
+
+        int n=nums.length;
+        ArrayList<Integer>dp=new ArrayList<>(Collections.nCopies(n,-1));
+        return f(n-1,nums,dp);
+    }
+
+    public int f(int idx, int[] nums, ArrayList<Integer> dp) {
+        if (idx == 0)
+            return nums[0];
+        if (idx < 0)
+            return 0;
+
+        if (dp.get(idx) != -1) {
+            return dp.get(idx);
         }
-        return prev1;
+        int pick = nums[idx] + f(idx - 2, nums, dp);
+        int notPick = 0 + f(idx - 1, nums, dp);
+
+        dp.set(idx, Math.max(pick, notPick));
+        return dp.get(idx);
     }
 }
