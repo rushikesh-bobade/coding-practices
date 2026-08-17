ddb5007490ccff2f850e8c3e013ac9cf419517da
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-17 23:55:11 +0530

    ✅ Combine Two Tables | Easy | SQL | Runtime: 719 ms (5.23%) | Memory: 0B | 2026-08-17

--- leetcode/easy/0175-combine-two-tables/solution.sql
+++ leetcode/easy/0175-combine-two-tables/solution.sql
@@ -1,16 +1,10 @@
 # Write your MySQL query statement below
-SELECT firstName,lastname,city,state
-from Person p
-LEFT JOIN Address a
+SELECT 
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person AS p
+LEFT JOIN 
+Address AS a
 ON p.personId=a.personId;
-
-
--- SELECT 
---     p.firstName,
---     p.lastName,
---     a.city,
---     a.state
--- FROM Person AS p
--- LEFT JOIN 
--- Address AS a
--- ON p.personId=a.personId;
