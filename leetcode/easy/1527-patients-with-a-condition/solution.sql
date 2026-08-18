5b62705fe8e45e01545809bb5a8d007d4a8a3229
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-18 21:43:38 +0530

    ✅ Patients With a Condition | Easy | SQL | Runtime: 469 ms (34.65%) | Memory: 0B | 2026-08-18

--- leetcode/easy/1527-patients-with-a-condition/solution.sql
+++ leetcode/easy/1527-patients-with-a-condition/solution.sql
@@ -1,7 +1,19 @@
 # Write your MySQL query statement below
-select patient_id,
-       patient_name,
-       conditions
-from Patients
-where  conditions like "DIAB1%"
-    OR conditions like "% DIAB1%";
+-- select patient_id,
+--        patient_name,
+--        conditions
+-- from Patients
+-- where  conditions like "DIAB1%"
+--     OR conditions like "% DIAB1%";
+
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions REGEXP '(^| )DIAB1';
+
+
+
+-- ^ → beginning of the conditions string
+-- | → OR
+-- → a space before the condition
+-- DIAB1 → required prefix
