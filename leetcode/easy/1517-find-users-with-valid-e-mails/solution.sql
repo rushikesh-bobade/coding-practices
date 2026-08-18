706a9cb16a4aef3e2eb9480bd8f8dd82de5b5a67
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-18 21:47:57 +0530

    ✅ Find Users With Valid E-Mails | Easy | SQL | Runtime: 92 ms | Memory: 0B | 2026-08-18

---
+++ leetcode/easy/1517-find-users-with-valid-e-mails/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT user_id, name, mail
+FROM Users
+WHERE mail 
+REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode.com$';
+
+-- How the pattern works
+-- ^ → start of email
+-- [A-Za-z] → first character must be a letter
+-- [A-Za-z0-9_.-]* → remaining prefix can contain letters, digits, _, ., -
+-- @leetcode\\.com → must end with exactly @leetcode.com
+-- $ → end of email
