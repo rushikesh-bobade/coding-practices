81d2c8dfed7613152871e8e7810d890753df29ac
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-18 15:31:39 +0530

    ✅ Fix Names in a Table | Easy | SQL | Runtime: 1071 ms (5.93%) | Memory: 0B | 2026-08-18

---
+++ leetcode/easy/1667-fix-names-in-a-table/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select user_id,
+    concat(
+        UPPER(LEFT(name,1)),
+        LOWER(substring(name,2))
+    ) as name
+from Users
+order by user_id;
