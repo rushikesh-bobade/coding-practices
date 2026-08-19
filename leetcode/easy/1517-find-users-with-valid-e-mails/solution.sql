1b8bef6ad48a312a56920fd480be713b76cf5efe
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-08-19 17:15:12 +0530

    ✅ Find Users With Valid E-Mails | Easy | SQL | Runtime: 932 ms (27.51%) | Memory: 0B | 2026-08-19

--- leetcode/easy/1517-find-users-with-valid-e-mails/solution.sql
+++ leetcode/easy/1517-find-users-with-valid-e-mails/solution.sql
@@ -7,13 +7,24 @@
 --     'c'
 -- );
 
-SELECT
-    *
-FROM
-    Users u
-WHERE
-    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
-    AND mail LIKE BINARY '%@leetcode.com'
+-- SELECT
+--     *
+-- FROM
+--     Users u
+-- WHERE
+--     mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
+--     AND mail LIKE BINARY '%@leetcode.com'
+
+
+SELECT user_id, name, mail
+FROM Users
+WHERE REGEXP_LIKE(
+    mail,
+    '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$',
+    'c'
+);
+
+
 
 -- How the pattern works
 -- ^ → start of email
